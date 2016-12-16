2ed3011db515816a122788179d018d6c09b7845c
bartes19 <bartes19@gmail.com>
2016-12-16 21:40:03 +0100

    JPA - Update

--- src/main/java/pl/edu/utp/model/Person.java
+++ src/main/java/pl/edu/utp/model/Person.java
@@ -4,14 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Person {
-
+    @Id
+    @GeneratedValue
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String surname;
+
+    @Column(nullable = false)
     private Integer age;
 
 }
