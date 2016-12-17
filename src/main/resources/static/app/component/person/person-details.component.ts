c113af5f0477d1ed712a4e5f5fc1f89296c4f981
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-12-17 12:04:12 +0100

    Update

--- src/main/resources/static/app/component/person/person-details.component.ts
+++ src/main/resources/static/app/component/person/person-details.component.ts
@@ -9,7 +9,17 @@ import {Person} from "../../model/Person";
 export class PersonDetailComponent  {
 
   person : Person;
+  ages = [1,2,3,4,5,6,7,8,9,10];
+
   ngOnInit() {
+    this.person = new Person(11, "Grześ", "Dębkowski", 11);
   }
+
+
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.person); }
 
 }
