777d1faa4db7645639a9be52b2f03ea865393ad8
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-12-28 17:53:11 +0100

    Update - Dodałem rdash

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -4,17 +4,42 @@ import {Person} from "./model/Person";
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent  {
 
-  persons : Person;
-
   ngOnInit() {
-
   }
 
+  mobileView:number = 992;
+  toggle:boolean = false;
 
-  name = 'Angular';
+  constructor() {
+    this.attachEvents();
+  }
+
+  attachEvents() {
+    window.onresize = ()=> {
+      if (this.getWidth() >= this.mobileView) {
+        if (localStorage.getItem('toggle')) {
+          this.toggle = !localStorage.getItem('toggle') ? false : true;
+        } else {
+          this.toggle = true;
+        }
+      } else {
+        this.toggle = false;
+      }
+    }
+  }
+
+  getWidth() {
+    return window.innerWidth;
+  }
+
+  toggleSidebar() {
+    this.toggle = !this.toggle;
+    localStorage.setItem('toggle', this.toggle.toString());
+  }
 
 }
